04f009730bc81c21fbafd4b89ace11e54ec041f3
Rolv-Apneseth <rolv.apneseth@gmail.com>
2022-02-14 22:12:52 +0000

    Basic testing for the functionality of the script

--- tests/test_daily_hn.py
+++ tests/test_daily_hn.py
@@ -1,5 +1,31 @@
-import daily_hn
+from daily_hn import Stories, UI
 
 
-def test_one():
-    assert True
+class TestDailyHN:
+    # Expensive call as it must scrape the newscombinator best stories page, so
+    # it is only made once
+    stories: list[dict] = Stories.get_stories()
+    assert stories
+
+    def test_Stories(self, capsys):
+        Stories.print_articles(stories=self.stories)
+
+        captured = capsys.readouterr()
+
+        assert len(captured.out.split("\n")) // 5 == len(self.stories)  # 5 lines per
+
+        for story in self.stories:
+            assert story["headline"] and story["link"] and story["score"]
+            assert story["link"].startswith("http")
+            assert type(story["score"]) is int
+
+        assert self.stories[0]["score"] >= self.stories[-1]["score"]
+
+    def test_UI_shortcuts(self):
+        assert len(UI.story_shortcuts) >= len(self.stories)
+
+    def test_UI_format_headlines(self):
+        headline = "headline"
+        formatted_headline = UI._format_headline(headline, 6)
+
+        assert formatted_headline == "hea..."
